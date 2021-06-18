47cbd5eb4ebc7aa16c94f6c5991178243c223796
Simon C <3833952+7imon7ays@users.noreply.github.com>
2021-06-18 19:26:37 -0400

    Replace pending logo

--- frontend/src/Pending.js
+++ frontend/src/Pending.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 
 export default function Pending (search) {
   return (
-    <img style={{ height: 'auto', width: '10%' }} src={logo} className="App-logo" alt="logo" />
+    <img style={{ height: '1.5em', width: '1.5em' }} src={logo} className="App-logo" alt="logo" />
     
   );
 }
